565fd6031b939a78aac5a96b10ad8d25efd17879
Malyna23 <46026189+Malyna23@users.noreply.github.com>
2019-03-12 00:02:35 -0700

    Update scenario_ip.sh

--- scenario_ip.sh
+++ scenario_ip.sh
@@ -1,8 +1,5 @@
 IPBALANCER=$1
-N=$2
 sudo sed -i "/server  app2 192.168.56.6:80 check/a\ server app $IPBALANCER:80 check" /etc/haproxy/haproxy.cfg
 
-
-#Install Haproxy
-
+#Restart Haproxy
 sudo systemctl restart haproxy
